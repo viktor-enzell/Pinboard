6708ae8531c00631dbf4576817056d53060ac333
viktor-enzell <viktor.enzell@gmail.com>
2018-12-11 11:25:14 +0100

    Removed console log

--- client/src/App.js
+++ client/src/App.js
@@ -75,7 +75,6 @@ class App extends Component {
     updatedNotes[note.ID] = note;
     this.setState({ notes: updatedNotes });
     this.version++;
-    console.log(this.state.notes);
   }
 
   handleHeaderChange = e => {
